da1cdb89ac1b17b97e72a55f8cfef14bfe098f52
leann-labra <leann.labra@gmail.com>
2023-01-07 12:50:47 -0800

    added socket.io js, and made some adjustments to roommatePage

---
+++ public/js/searchRoommate.js
@@ -0,0 +1,10 @@
+const searchEl = document.getElementById("search-btn");
+
+// get roommate name fetch request
+
+// grab roommate post
+
+searchEl.addEventListener("click", function () {
+  const searchTerm = inputEl.value;
+  // render roommate post
+});
