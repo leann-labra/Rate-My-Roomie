851665fe9ef8e8264b987b31d39fba866f2b37f0
leann-labra <leann.labra@gmail.com>
2023-01-08 15:25:06 -0800

    made changes to prevPost and post js

--- public/js/roommatePost.js
+++ public/js/roommatePost.js
@@ -1,25 +1,29 @@
 // create a fetch request to grab roommate posts
 //and append new posts on top of each other
 async function createPost(e) {
-    e.preventDefault();
-  
-    const name = document.getElementById("post-title roommateName").value;
-    const post_content = document.getElementById("post-description").value;
-  
-    const res = await fetch("/api/roommatePost", {
-      method: "POST",
-      body: JSON.stringify({
-        name,
-        post_content,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  
-    if (res.ok) {
-      document.location.replace("/roommatePage");
-    } else {
-      alert("Unable to create new post!");
-    }
+  e.preventDefault();
+
+  const name = document.getElementById("post-title roommateName").value;
+  const post_content = document.getElementById("post-description").value;
+
+  const res = await fetch("/api/roommatePost", {
+    method: "POST",
+    body: JSON.stringify({
+      post_title,
+      name,
+      city,
+      county,
+      age,
+      lease_length
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (res.ok) {
+    document.location.replace("/roommatePage");
+  } else {
+    alert("Unable to create new post!");
   }
-  
-  document.getElementById("create-post").addEventListener("submit", createPost);
+}
+
+document.getElementById("create-post").addEventListener("submit", createPost);
