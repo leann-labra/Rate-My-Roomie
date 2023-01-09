cb8425ab61b54e8ba8277b0aaf1be2ff075cfb66
Cherie2 <cwalk280@yahoo.com>
2023-01-09 15:14:01 -0800

    ./pageRoute bug fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const user = require("./userRoute");
 const post = require("./roommatePost");
-const roommatePage = require("./pageRoute");
 
-router.use("/roommatepage", roommatePage);
 router.use("/post", post);
 router.use("/user", user);
 
