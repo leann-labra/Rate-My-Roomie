1d1e4eab87e074b43cf0f588e41da32ebf6de116
leann-labra <leann.labra@gmail.com>
2023-01-09 18:28:12 -0800

    made styling changes to roommate page, fixed post route to render all info

--- controllers/pageRoute.js
+++ controllers/pageRoute.js
@@ -12,9 +12,9 @@ router.get("/", async (req, res) => {
         "last_name",
         "county",
         "city",
-        // "date_created",
+        "age",
+        "lease_length",
       ],
-      //   order: [["date_created", "DESC"]],
       include: {
         model: User,
         attributes: ["name"],
@@ -30,101 +30,38 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const postData = await Post.findbyPk({
-//       where: {
-//         id: req.params.id,
-//       },
-//       attributes: [
-//         "id",
-//         "title",
-//         "first_name",
-//         "last_name",
-//         "county",
-//         "city",
-//         "date_created",
-//       ],
-//       order: [["date_created", "DESC"]],
-//       include: {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     });
-//     console.log(postData);
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findbyPk({
+      where: {
+        id: req.params.id,
+      },
+      attributes: [
+        "id",
+        "title",
+        "first_name",
+        "last_name",
+        "county",
+        "city",
+        "date_created",
+      ],
+      order: [["date_created", "DESC"]],
+      include: {
+        model: User,
+        attributes: ["username"],
+      },
+    });
+    console.log(postData);
 
-//     if (!postData) {
-//       res.status(404).json({ message: "No post found with this id" });
-//       return;
-//     }
-//     res.status(200).json(postData);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-// //when you sign up, will direct you to roommatePage
-// //gettng Post inputs from addRoommate page
-// router.get("/addRoommate", async (req, res) => {
-//   try {
-//     const postData = await Post.findAll({
-//       attributes: [
-//         "id",
-//         "title",
-//         "first_name",
-//         "last_name",
-//         "county",
-//         "city",
-//         "date_created",
-//       ],
-//       order: [["date_created", "DESC"]],
-//       include: {
-//         model: User,
-//         attributes: ["name"],
-//       },
-//     });
-//     console.log(postData);
-//     const posts = postData.map((post) => post.get({ plain: true }));
-
-//     res.render("roommatePage", { posts });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const postData = await Post.findbyPk({
-//       where: {
-//         id: req.params.id,
-//       },
-//       attributes: [
-//         "id",
-//         "title",
-//         "first_name",
-//         "last_name",
-//         "county",
-//         "city",
-//         // "date_created",
-//       ],
-//       //   order: [["date_created", "DESC"]],
-//       include: {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     });
-//     console.log(postData);
-
-//     if (!postData) {
-//       res.status(404).json({ message: "No post found with this id" });
-//       return;
-//     }
-//     res.status(200).json(postData);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
 module.exports = router;
